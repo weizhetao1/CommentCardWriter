de43b6bbbe164ab7c7dbcd955f0889b987b4c81f
William Tao <tao.w@etoncollege.org.uk>
2022-02-04 11:58:20 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Tao, Weizhe (Coll) on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentCardWriter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CommentCardWriter
+//
+//  Created by Tao, Weizhe (Coll) on 04/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
